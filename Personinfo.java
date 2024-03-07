d30333046ca53086ff34fc463c34499c899779cc
shahzoddeveloper <abdujalilovvip1991@gmail.com>
2024-03-07 20:42:48 +0100

    Delete Personinfo.java

--- Personinfo.java
+++
@@ -1,8 +0,0 @@
-// OPPGAVE 01.02
-
-class Personinfo{
-    public static void main(String[] args){
-        System.out.println("Ole Brum");
-        System.out.println("Kjendisveien 777B");
-    }
-}
